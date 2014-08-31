473f2809482c7c3a4c0e0f1ecaad4d58be98c472
Arvind Menon <amenon81@gmail.com>
2014-08-30 20:58:36 -0400

    Cleaning imports

--- demo/src/main/java/com/daimajia/swipedemo/GridViewExample.java
+++ demo/src/main/java/com/daimajia/swipedemo/GridViewExample.java
@@ -1,12 +1,9 @@
 package com.daimajia.swipedemo;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
