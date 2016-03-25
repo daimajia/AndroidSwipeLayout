c7f44dd99a7600a1d6ce8d2369d491db332c3b11
ZhongTing <gary62107@gmail.com>
2016-03-25 18:12:40 +0800

    #289 fix bug - long press doesn't trigger onClick event

--- library/src/main/java/com/daimajia/swipe/SwipeLayout.java
+++ library/src/main/java/com/daimajia/swipe/SwipeLayout.java
@@ -1147,8 +1147,7 @@ public class SwipeLayout extends FrameLayout {
                 setOnLongClickListener(new OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
-                        performAdapterViewItemLongClick();
-                        return true;
+                        return performAdapterViewItemLongClick();
                     }
                 });
             }
